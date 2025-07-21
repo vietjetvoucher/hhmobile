be3e93c3cb6da532a6021d596e9f33ed4c1cd4df
vietjetvoucher <dimensiongsv@gmail.com>
2025-07-21 10:20:04 +0700

    script.js

--- script.js
+++ script.js
@@ -1270,9 +1270,9 @@ function debounce(func, delay) {
 }
 
 // Debounced versions of renderOrders for search inputs
-const debouncedRenderCreatedOrders = debounce(() => renderOrders('created'), 2000);
-const debouncedRenderShippingOrders = debounce(() => renderOrders('shipping'), 2000);
-const debouncedRenderDeliveredOrders = debounce(() => renderOrders('delivered'), 2000);
+const debouncedRenderCreatedOrders = debounce(() => renderOrders('created'), 1500);
+const debouncedRenderShippingOrders = debounce(() => renderOrders('shipping'), 1500);
+const debouncedRenderDeliveredOrders = debounce(() => renderOrders('delivered'), 1500);
 
 async function renderOrders(status) {
     const orderListElement = document.getElementById(`${status}-orders-list`);
